f7b8fe679497d60497f476de8860934b464a9562
Michael Jones <mikej253@me.com>
2015-06-10 11:57:50 +1000

    Basic Web API get call using jersey client. Unit test running.

---
+++ src/main/java/com/michaeljones/httpclient/jersey/JerseyMethod.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.michaeljones.httpclient.jersey;
+
+import com.michaeljones.httpclient.HttpMethod;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+
+
+/**
+ *
+ * @author michaeljones
+ */
+public class JerseyMethod implements HttpMethod {
+    Client jerseyImpl;
+    
+    public JerseyMethod() {
+        jerseyImpl = Client.create();
+    }
+
+    @Override
+    public String GetStringContent(String url) {
+        WebResource resource = jerseyImpl.resource(url);
+        ClientResponse response = resource.accept("application/json").get(ClientResponse.class);
+        if (response.getStatus() != 200) {
+            throw new RuntimeException("Jersey client failed : HTTP error code : " + response.getStatus());
+        }
+        
+        String result = response.getEntity(String.class);
+        
+        return result;
+    }   
+}
